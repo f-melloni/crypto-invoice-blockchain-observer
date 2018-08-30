49b12c624899893e9c175cca5328ee983db6003d
Fabio Melloni <fabio.melloni@futuresolutionservices.com>
2018-08-30 14:09:10 +0200

    Use better Sentry client

--- BlockchainObserver/Program.cs
+++ BlockchainObserver/Program.cs
@@ -20,6 +20,7 @@ namespace BlockchainObserver
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseSentry()
                 .Build();
     }
 }
